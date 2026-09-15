e97ab58ca97cfaa9afdfbfb626ffcca1b78dded5
Abdul-Malik-spl <abdulmalikspl@gmail.com>
2026-09-15 12:02:50 +0530

    first commit

---
+++ flames.js
@@ -0,0 +1,78 @@
+let con=document.getElementById("content")
+let name=document.getElementById("name")
+let oname=document.getElementById("oname")
+let fname=document.getElementById("f-name")
+let sname=document.getElementById("s-name")
+let ans=document.getElementById("ans")
+let counts=document.getElementById("counts")
+let result=document.getElementById("result")
+let arr=[{key:"F",value:"Friends",active:true},{key:"L",value:"Love",active:true},{key:"A",value:"Affection",active:true},{key:"M",value:"Marriage",active:true},{key:"E",value:"Enemy",active:true},{key:"S",value:"Sister",active:true}]
+let nm=[]
+let onm=[]
+let first;
+let second;
+// let ccarr=[]
+// let duplet={}
+
+
+function check(){
+    // if(!name.trim||!oname.trim){
+    //      errorMsg()
+    //      return
+    // }
+nm=name.value.toUpperCase().replace(/\s/g, "").split("").map(a=>{return {v:a,active:true}})
+onm=oname.value.toUpperCase().replace(/\s/g, "").split("").map(a=>{return {v:a,active:true}})
+console.log(nm,onm)
+findDuplicats()
+}
+function errorMsg(){
+alert(`please enter name and other name `)
+}
+function findDuplicats(){
+//    let ans= ccarr.reduce((obj,v,ind)=>{
+//         return ind!==ccarr.indexOf(v)?{...obj,[v]:obj[v]+1}:{...obj,[v]:1}
+//     },{})
+//   duplet=ans
+first=nm.length<=onm.length?nm:onm
+second=nm.length<=onm.length?onm:nm
+for(let i=0;i<first.length;i++){
+for(let j=0;j<second.length;j++){
+if(first[i].v==second[j].v&&(first[i].active&&second[j].active)){
+    first[i].active=false
+    second[j].active=false
+}
+}
+}
+showData()
+}
+
+function showData(){
+    ans.style.display="flex"
+    counts.style.display="flex"
+    let vl=first.filter(a=>a.active).length+second.filter(a=>a.active).length
+    let counting=0
+    let intv=setInterval(()=>{
+        counting++
+        counts.innerHTML=`${counting} `
+        if(counting==vl){
+            clearInterval(intv)
+            result.style.display="flex"
+            showingResult()
+        }
+    },100)
+    first.forEach((obj)=>{
+        let span=document.createElement("span")
+        span.innerHTML=obj.v
+        span.className=`${obj.active?'':'same'}`
+        fname.appendChild(span)
+    })
+       second.forEach((obj)=>{
+        let span=document.createElement("span")
+        span.innerHTML=obj.v
+        span.className=`${obj.active?'':'same'}`
+        sname.appendChild(span)
+    })
+}
+function showingResult(){
+
+}
